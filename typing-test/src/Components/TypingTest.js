2a613c81304b374b04d5e101acafd854739916f5
Aarav Bajaj <aaravbajaj08@gmail.com>
2024-12-05 13:38:25 +1100

    initial typing test commit

---
+++ typing-test/src/Components/TypingTest.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TypingTest() {
+  return (
+    <div>
+      <div className="test"></div>
+      <div className="result">
+        <p>Time Left: </p>
+        <p>Mistakes: </p>
+        <p>WPM: </p>
+        <p>CPM: </p>
+      </div>
+    </div>
+  );
+}
+export default TypingTest;
